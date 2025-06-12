ee931b0cbcc79dd431f9794041e01128014c3850
Charankumar-123 <126707433+Charankumar-123@users.noreply.github.com>
2025-06-12 12:18:56 +0530

    12

--- DHA/src/main/java/com/ac/dha/repository/WebServiceMethodRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.ac.dha.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import com.ac.dha.utils.WebServiceEntitiesMapper;
-//
-//public interface WebServiceMethodRepository extends JpaRepository<WebServiceEntitiesMapper, Integer>{
-//
-//}
